15c8d2dbb2c5aef893d623a4e6c504dcba96a62b
DChang87 <diana.chang.twn@gmail.com>
2015-01-28 11:36:08 -0500

    Updated GamePanel

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -25,52 +25,24 @@ class GamePanel extends JPanel implements KeyListener{
 	
 	//obj
 	private ArrayList<Integer> goals=new ArrayList<Integer>();
-	private ArrayList<Integer> objx = new ArrayList<Integer>();
-	private ArrayList<Integer> objy = new ArrayList<Integer>();
-	private ArrayList<Integer> objv = new ArrayList<Integer>();
-	private ArrayList<Double> objs = new ArrayList<Double>();
-	private ArrayList<Image> objsprites = new ArrayList<Image>();
-	//
-	private int numobj=4;
 	private Image background,obj1;
 	private final int NOobj=-1;
 	private int objcaught=-1;
 	private int totals=0;
 	private int totalval=0;
 	private int level=1;
+	public Objects obj = new Objects();
 	public GamePanel(Miner m){
 		keys = new boolean[65535];
+		goals.add(1);
 		background = new ImageIcon("goldminer1.jpg").getImage();
-		addThings();
 		mainFrame=m;
 		startx=400;
 		starty=90;
 		setSize(800,640);
 		addKeyListener(this);
 	}
-	public void addThings(){
-		objsprites.add(new ImageIcon("gold2.png").getImage());
-		objx.add(100);
-		objy.add(200);
-		objv.add(50);
-		objs.add(-1.0);
-		objsprites.add(new ImageIcon("gold1.png").getImage());
-		objx.add(300);
-		objy.add(400);
-		objs.add(-0.25);
-		objv.add(200);
-		objsprites.add(new ImageIcon("rock1.png").getImage());
-		objx.add(200);
-		objy.add(300);
-		objs.add(-0.25);
-		objv.add(10);
-		objsprites.add(new ImageIcon("rock2.png").getImage());
-		objx.add(600);
-		objy.add(300);
-		objs.add(-0.5);
-		objv.add(5);
-		goals.add(220);
-	}
+	
 	public void addNotify() {
         super.addNotify();
         requestFocus();
@@ -93,21 +65,20 @@ class GamePanel extends JPanel implements KeyListener{
 		else if (shootdire!= up && length<maxlength){
 			shootdire=down;
 		}
-		
 		if (objcaught!=-1){
 			//this means that a piece of obj has been caught
-			moveobj();
-			shootdire=objs.get(objcaught);
+			obj.move(objcaught,endx,endy);
+			shootdire=obj.speed.get(objcaught);
 			if (length==50){
 				System.out.println("length"+length);
-				totalval+=objv.get(objcaught);
-				removeVal();
-				numobj--;
+				totalval+=obj.val.get(objcaught);
+				obj.removeVal(objcaught);
+				obj.numobj--;
 				objcaught=-1;
 			}
 		}
 		else{
-			objcaught = catchobj();
+			objcaught = obj.catchobj(endx,endy);
 		}
 		//System.out.println(objcaught+" objcaught");
 		//move
@@ -124,35 +95,9 @@ class GamePanel extends JPanel implements KeyListener{
 			angle +=direction;
 		}
 	}
-	public void removeVal(){
-		objx.remove(objcaught);
-		objy.remove(objcaught);
-		objv.remove(objcaught);
-		objsprites.remove(objcaught);
-	}
-	public void moveobj(){
-		//this method moves the obj that is caught with the clamp
-		//System.out.println("moveobj");
-		objx.set(objcaught,endx-objsprites.get(objcaught).getWidth(null)/2);
-		objy.set(objcaught,endy-objsprites.get(objcaught).getHeight(null)/2);
-	}
-	public int catchobj(){
-		for (int i=0;i<numobj;i++){
-			if (distance(endy,objy.get(i)+objsprites.get(i).getHeight(null)/2)<=64&&distance(endx,objx.get(i)+objsprites.get(i).getWidth(null)/2)<=72){
-				System.out.println(distance(endy,objy.get(i))+" "+objsprites.get(i).getHeight(null)/2);
-				System.out.println(distance(endx,objx.get(i))+" "+objsprites.get(i).getWidth(null)/2);
-				return i;
-			}
-			if (distance(endy,objy.get(i))<=objsprites.get(i).getHeight(null)/2 && distance(endx,objx.get(i))<=objsprites.get(i).getWidth(null)/2){
-				return i; 
-			}
-			
-		}
-		return NOobj;
-	}
-	public int distance(int a,int b){
-		return Math.abs(b-a);
-	}
+	
+	
+
 	public void changeTime(){
 		totals++;
 	}
@@ -172,8 +117,8 @@ class GamePanel extends JPanel implements KeyListener{
 		g.drawLine(startx,starty,endx,endy);
 		g.setColor(Color.yellow);
 		Graphics2D g2d = (Graphics2D)g;
-		for (int i=0;i<numobj;i++){
-			g.drawImage(objsprites.get(i),objx.get(i),objy.get(i),this);
+		for (int i=0;i<obj.numobj;i++){
+			g.drawImage(obj.sprites.get(i),obj.x_val.get(i),obj.y_val.get(i),this);
 		}
 		g.setColor(Color.black);
 		
