f7924ee6bb528d8ce5c26b47033be7576c39baf9
DChang87 <diana.chang.twn@gmail.com>
2015-02-08 01:41:35 -0500

    Added read-file to read level information, fixed a few bugs

--- src/Objects.java
+++ src/Objects.java
@@ -4,15 +4,11 @@ import java.awt.geom.*;
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 import java.awt.Image;
-
 import javax.swing.*;
-
 import java.awt.image.*; 
 import java.io.*; 
-
 import javax.imageio.*; 
 import javax.swing.JPanel;
-
 import java.util.*;
 
 class Objects {
@@ -21,68 +17,58 @@ class Objects {
 	public static ArrayList<Integer> val = new ArrayList<Integer>();
 	public static ArrayList<Double> speed = new ArrayList<Double>();
 	public static ArrayList<Image> sprites = new ArrayList<Image>();
+	public static ArrayList<Integer> direction = new ArrayList<Integer>();
+	public static ArrayList<Integer> type = new ArrayList<Integer>();
+	public static int numobj=6; //CHANGE TO 0
+	public static int numTNT;
 	public static int numlevels=2;
+	public static int current_level;
 	public static final int GOLD = 1, ROCK = 2;
 	public static final int PIG=3, LEFT = 0, RIGHT=1,NODIRECTION=2;
-	public static ArrayList<Integer> type = new ArrayList<Integer>();
 	public static boolean [] pigsrunning = new boolean[2];
-	public static ArrayList<Integer> direction = new ArrayList<Integer>();
 	private final static int NOobj=-1;
-	public static int numobj=6;
 	public Objects(){
-		addThings();
+		current_level=1;
+		pigleft = new ImageIcon("pig.png").getImage();
+		pigright= new ImageIcon("pig2.png").getImage();
+		pigsrunning[0]=false;
+		pigsrunning[1]=true;
+		loadMyStuff(current_level);
 	}
 	private static Image pigleft;
 	private static Image pigright;
-	public void addThings(){
-		pigsrunning[0]=false;
-		pigsrunning[1]=true;
-		pigleft = new ImageIcon("pig.png").getImage();
-		pigright= new ImageIcon("pig2.png").getImage();
-		sprites.add(new ImageIcon("gold2.png").getImage());
-		type.add(GOLD);
-		x_val.add(100);
-		y_val.add(200);
-		val.add(50);
-		speed.add(-1.0);
-		direction.add(NODIRECTION);
-		sprites.add(new ImageIcon("gold1.png").getImage());
-		x_val.add(300);
-		type.add(GOLD);
-		y_val.add(400);
-		speed.add(-0.25);
-		val.add(200);
-		direction.add(NODIRECTION);
-		sprites.add(new ImageIcon("rock1.png").getImage());
-		x_val.add(200);
-		type.add(ROCK);
-		y_val.add(300);
-		speed.add(-0.25);
-		val.add(10);
-		direction.add(NODIRECTION);
-		sprites.add(new ImageIcon("rock2.png").getImage());
-		x_val.add(600);
-		type.add(ROCK);
-		y_val.add(300);
-		speed.add(-0.5);
-		direction.add(NODIRECTION);
-		val.add(5);
-		sprites.add(pigleft);
-		x_val.add(600);
-		y_val.add(400);
-		val.add(5);
-		speed.add(-2.0);
-		type.add(PIG);
-		direction.add(LEFT);
-		sprites.add(pigright);
-		x_val.add(200);
-		y_val.add(200);
-		val.add(5);
-		speed.add(-2.0);
-		type.add(PIG);
-		direction.add(RIGHT);
+	public static void loadMyStuff(int level){
+		Scanner infile = null;
+		try{
+			infile = new Scanner(new File("Data"+level+".txt"));
+		}
+		catch(IOException ex){
+			System.out.println("System cannot find Data"+level+".txt");
+		}
+		numobj = Integer.parseInt(infile.nextLine());
+		for (int i=0;i<numobj;i++){
+			x_val.add(infile.nextInt());
+		}
+		for (int i=0;i<numobj;i++){
+			y_val.add(infile.nextInt());
+		}
+		for (int i=0;i<numobj;i++){
+			val.add(infile.nextInt());
+		}
+		for (int i=0;i<numobj;i++){
+			speed.add(infile.nextDouble());
+		}
+		for (int i=0;i<numobj;i++){
+			sprites.add(new ImageIcon(infile.next()).getImage());
+		}
+		for (int i=0;i<numobj;i++){
+			direction.add(infile.nextInt());
+		}
+		for (int i=0;i<numobj;i++){
+			type.add(infile.nextInt());
+		}
+		numTNT = Integer.parseInt(infile.nextLine());
 	}
-	//pig flies downward
 	public static int catchobj(int endx, int endy){
 		for (int i=0;i<numobj;i++){
 			if (distance(endy,y_val.get(i)+sprites.get(i).getHeight(null)/2)<=sprites.get(i).getHeight(null)/2 && distance(endx,x_val.get(i)+sprites.get(i).getWidth(null)/2)<=sprites.get(i).getWidth(null)/2){
