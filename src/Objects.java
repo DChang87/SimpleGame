234e2eaba7e6f61dc3721d537b3aa0612ba82809
DChang87 <diana.chang.twn@gmail.com>
2015-02-08 16:27:22 -0500

    basic TNT

--- src/Objects.java
+++ src/Objects.java
@@ -4,156 +4,209 @@ import java.awt.geom.*;
 import java.awt.Font;
 import java.awt.GraphicsEnvironment;
 import java.awt.Image;
+
 import javax.swing.*;
+
 import java.awt.image.*; 
 import java.io.*; 
+
 import javax.imageio.*; 
 import javax.swing.JPanel;
+
 import java.util.*;
 
-class Objects {
-	public static ArrayList<Integer> x_val = new ArrayList<Integer>();
-	public static ArrayList<Integer> y_val = new ArrayList<Integer>();
-	public static ArrayList<Integer> val = new ArrayList<Integer>();
-	public static ArrayList<Double> speed = new ArrayList<Double>();
-	public static ArrayList<Image> sprites = new ArrayList<Image>();
-	public static ArrayList<Integer> direction = new ArrayList<Integer>();
-	public static ArrayList<Integer> type = new ArrayList<Integer>();
-	public static ArrayList<Integer> TNT_x = new ArrayList<Integer>();
-	public static ArrayList<Integer> TNT_y = new ArrayList<Integer>();
-	
-	public static int numobj=6; //CHANGE TO 0
-	public static int numTNT;
-	public static int numlevels=2;
-	public static int current_level;
-	public static final int GOLD = 1, ROCK = 2;
-	public static final int PIG=3, LEFT = 0, RIGHT=1,NODIRECTION=2;
-	public static boolean [] pigsrunning = new boolean[2];
-	private final static int NOobj=-1;
-	public Objects(){
-		current_level=1;
-		pigleft = new ImageIcon("pig.png").getImage();
-		pigright= new ImageIcon("pig2.png").getImage();
-		pigsrunning[0]=false;
-		pigsrunning[1]=true;
-		loadMyStuff(current_level);
-	}
-	private static Image pigleft;
-	private static Image pigright;
-	public static void loadMyStuff(int level){
-		Scanner infile = null;
-		try{
-			infile = new Scanner(new File("Data"+level+".txt"));
-		}
-		catch(IOException ex){
-			System.out.println("System cannot find Data"+level+".txt");
-		}
-		numobj = Integer.parseInt(infile.nextLine());
-		for (int i=0;i<numobj;i++){
-			x_val.add(infile.nextInt());
-		}
-		for (int i=0;i<numobj;i++){
-			y_val.add(infile.nextInt());
-		}
-		for (int i=0;i<numobj;i++){
-			val.add(infile.nextInt());
-		}
-		for (int i=0;i<numobj;i++){
-			speed.add(infile.nextDouble());
-		}
-		for (int i=0;i<numobj;i++){
-			sprites.add(new ImageIcon(infile.next()).getImage());
-		}
-		for (int i=0;i<numobj;i++){
-			direction.add(infile.nextInt());
-		}
-		for (int i=0;i<numobj;i++){
-			type.add(infile.nextInt());
-		}
-		numTNT = Integer.parseInt(infile.nextLine());
-		for (int i=0;i<numTNT;i++){
-			TNT_x.add(infile.nextInt());
-		}
-		for (int i=0;i<numTNT;i++){
-			TNT_y.add(infile.nextInt());
-		}
-		//read in x,y position of TNT since all TNT are equal in power
-	}
-	public static int catchobj(int endx, int endy){
-		for (int i=0;i<numobj;i++){
-			if (distance(endy,y_val.get(i)+sprites.get(i).getHeight(null)/2)<=sprites.get(i).getHeight(null)/2 && distance(endx,x_val.get(i)+sprites.get(i).getWidth(null)/2)<=sprites.get(i).getWidth(null)/2){
-				System.out.println(i+"OBJECTCAUGHT");
-				return i; 
-			}	
-		}
-		return NOobj;
-	}
-	public static int distance(int a,int b){
-		return Math.abs(b-a);
-	}
-	public static double distance_2(int x1,int y1,int x2,int y2){
-		return Math.sqrt((Math.pow(distance(x1,x2),2)+Math.pow(distance(y1,y2),2)));
-	}
-	public static void checkTNT(int check_Val){
-		ArrayList<Integer> to_Remove = new ArrayList<Integer>();
-		
-		//blit exploding pic
-		//75 is current explosion radius
-		for (int i=0;i<numobj;i++){
-			if (distance_2(x_val.get(i),y_val.get(i),TNT_x.get(check_Val),TNT_y.get(check_Val))<75){
-				to_Remove.add(i);
-			}
-		}
-		
-		for (int i=0;i<to_Remove.size();i++){
-			removeVal(to_Remove.get(i));
-		}
-		GamePanel.exploded_TNT.add(check_Val);
-		int x = TNT_x.get(check_Val);
-		int y = TNT_y.get(check_Val);
-		for (int i=0;i<numTNT;i++){
-			if (i!=check_Val & distance_2(TNT_x.get(check_Val),TNT_y.get(check_Val),TNT_x.get(i),TNT_y.get(i))<75){
-				checkTNT(i);
-			}
-		}
-		TNT_x.remove(new Integer(x));
-		TNT_y.remove(new Integer(y));
-		numTNT-=1;
-	}
-	public static void removeVal(int objcaught){
-		x_val.remove(objcaught);
-		y_val.remove(objcaught);
-		val.remove(objcaught);
-		sprites.remove(objcaught);
-		type.remove(objcaught);
-		numobj-=1;
-	}
-	public static void move(int objcaught, int endx,int endy){
-		//this method moves the obj that is caught with the clamp
-		//System.out.println("moveobj");
-		x_val.set(objcaught,endx-sprites.get(objcaught).getWidth(null)/2);
-		y_val.set(objcaught,endy-sprites.get(objcaught).getHeight(null)/2);
+class GamePanel extends JPanel implements KeyListener{
+	private final int RIGHT=1,LEFT=-1;
+	private int endx,endy,direction=LEFT,angle=20,startx,starty;
+	private boolean[] keys;
+	//old down = 3, up = -0.5
+	private double down=5,up=-1.5,shootdire=down,length=50;
+	private boolean shooting;
+	public boolean ready=false;
+	private Miner mainFrame;
+	//obj
+	private ArrayList<Integer> goals=new ArrayList<Integer>();
+	private int numDynamites;
+	private static Image dynamitePic = new ImageIcon("dynamite.png").getImage();
+	public static Image TNT_Image = new ImageIcon("tnt.png").getImage();
+	private static Image boomPic = new ImageIcon("boom.png").getImage();
+	public static ArrayList<ArrayList<Integer>> explode_nextRound = new ArrayList<ArrayList<Integer>>(); //all of the TNT that should be exploded
+	public static ArrayList<ArrayList<Integer>> explode_thisRound = new ArrayList<ArrayList<Integer>>();
+	private Image background;
+	private int TNT_timer=0;
+	private int boom_timer = -1;
+	private int objcaught=-1;
+	private int totals=200;
+	private int totalval=0;
+	public Objects obj = new Objects();
+	public GamePanel(Miner m){
+		numDynamites=3;
+		keys = new boolean[65535];
+		goals.add(1);
+		background = new ImageIcon("goldminer1.jpg").getImage();
+		mainFrame=m;
+		startx=400;
+		starty=90;
+		setSize(800,640);
+		addKeyListener(this);
 	}
 	
-	public static void movePigs(int objcaught){
-		for (int i=0;i<type.size();i++){
-			if (type.get(i)==PIG && i!=objcaught){
-				if (direction.get(i)==LEFT){
-					x_val.set(i,x_val.get(i)-2);
-					if (x_val.get(i)<=0){
-						sprites.set(i,pigright);
-						direction.set(i,RIGHT);
-					}
-				}
-				else{
-					x_val.set(i,x_val.get(i)+2);
-					if( x_val.get(i)>=800-sprites.get(i).getWidth(null)){
-						sprites.set(i, pigleft);
-						direction.set(i,LEFT);
-					}
-				}
+	public void addNotify() {
+        super.addNotify();
+        requestFocus();
+        mainFrame.start();
+    }
+	public void move(){
+		//swing
+		obj.movePigs(objcaught);
+		double maxlength=400;
+		if (angle>=160){
+			direction=LEFT;
+		}
+		else if (angle<=20){
+			direction=RIGHT;
+		}
+		//shoot
+		if (length>=maxlength){
+			shootdire=up;
+		}
+		else if (shootdire!= up && length<maxlength){
+			shootdire=down;
+		}
+		//calling old TNT's and checking for new exploding ones that the user reached
+		for (int i=0;i<obj.numTNT;i++){
+			//System.out.println("LOOP WITH TNT");
+			if (obj.distance(obj.TNT_pos.get(i).get(0)+TNT_Image.getWidth(null)/2,endx)<=TNT_Image.getWidth(null)/2 &&obj.distance(obj.TNT_pos.get(i).get(1)+TNT_Image.getHeight(null)/2,endx)<=TNT_Image.getHeight(null)/2){
+				explode_thisRound.add(obj.TNT_pos.get(i));
 			}
 		}
+		for (int i=0;i<explode_thisRound.size();i++){
+			//get the co-ords at explode_thisRound, find the index at TNT_pos and call checkTNT on it to add more things to explode_nextRound;
+			System.out.println("THIS ROUND LOOP"+explode_thisRound.size());
+			obj.checkTNT(obj.TNT_pos.indexOf(explode_thisRound.get(i)));
+		}
+		
+		if (objcaught!=-1){
+			//this means that a piece of obj has been caught
+			obj.move(objcaught,endx,endy);
+			shootdire=obj.speed.get(objcaught);
+			if (length==50){
+				totalval+=obj.integer_data.get(objcaught).get(obj.VAL);
+				obj.removeVal(objcaught);
+				objcaught=-1;
+			}
+		}
+		else{
+			if (shootdire==down){
+				objcaught = obj.catchobj(endx,endy);
+			}
+		}
+		if (shooting){
+			length=Math.min(length+shootdire,maxlength);
+			length=Math.max(length+shootdire,50);
+			if (length==50){
+				shooting=false;
+				shootdire=down;
+			}
+		}
+		else{
+			shooting = keys[KeyEvent.VK_SPACE];
+			angle +=direction;
+		}
+	}
+	
+	
+	public void throwDynamite(){
+		if (keys[KeyEvent.VK_D] && numDynamites>0){
+			if (objcaught!=-1){
+				obj.removeVal(objcaught);
+				numDynamites-=1;
+				objcaught=-1;
+			}
+		shootdire = up;
+		}
+	}
+
+	public void changeTime(){
+		//System.out.println("changetime");
+		if (totals==0){
+			totals=1234;
+		}
+		totals--;
+		if (boom_timer!=-1){
+			System.out.println("changetime yo");
+			boom_timer+=1;
+		}
+	}
+    // --------- Keystuff---------------
+    public void keyTyped(KeyEvent e){}
+    public void keyPressed(KeyEvent e){
+    	keys[e.getKeyCode()]=true;
+    }
+    public void keyReleased(KeyEvent e){
+    	keys[e.getKeyCode()]=false;
+    }
+	public void paintComponent(Graphics g){
+		//System.out.println("paint"+TNT_timer);
+		g.drawImage(background,0,0,this);  
+		g.setColor(Color.black);
+		endx=startx+(int)(length*Math.cos(Math.toRadians(angle)));
+		endy=starty+(int)(length*(Math.sin(Math.toRadians(angle))));
+		g.drawLine(startx,starty,endx,endy);
+		g.setColor(Color.yellow);
+		Graphics2D g2d = (Graphics2D)g;
+		for (int i=0;i<obj.numobj;i++){
+			g.drawImage(obj.sprites.get(i),obj.integer_data.get(i).get(obj.X),obj.integer_data.get(i).get(obj.Y),this);
+		}
+		for (int i=0;i<numDynamites;i++){
+			g.drawImage(dynamitePic,460+i*35,85,this);
+		}
+		for (int i=0;i<obj.numTNT;i++){
+			g.drawImage(TNT_Image,obj.TNT_pos.get(i).get(0),obj.TNT_pos.get(i).get(1),this);
+		}
+		g.setColor(Color.black);
+		//g.drawOval(300+TNT_Image.getWidth(null)-150, 300+TNT_Image.getHeight(null)-150, 300, 300);
+		//g.drawOval(300-100, 300-100, 200, 200);
+		//g.drawOval(300+TNT_Image.getWidth(null)/2,300+TNT_Image.getHeight(null)/2,20,20);
+		//g.drawOval(300+TNT_Image.getWidth(null)/2-200, 300+TNT_Image.getHeight(null)/2-200, 200, 200);
+		//drawing strings
+		Font font = new Font("Calisto MT", Font.PLAIN, 20);
+		g.setFont(font);
+		g.drawString("Time: "+totals, 680, 55);
+		g.drawString("Goal: "+goals.get(obj.current_level-1),20,90);
+		g.drawString("current level: "+obj.current_level,680,90);
+		g.drawString("Money: "+totalval, 20, 55);
+		//System.out.println("timer"+boom_timer+explode_thisRound.size());
+		for (int i=0;i<explode_thisRound.size();i++){
+			//System.out.println("explode?");
+			if (boom_timer<1){
+				g.drawImage(boomPic,explode_thisRound.get(i).get(0)+TNT_Image.getWidth(null)/2-boomPic.getWidth(null)/2,explode_thisRound.get(i).get(1)+TNT_Image.getHeight(null)/2-boomPic.getHeight(null)/2,this);
+				//ystem.out.println("explode?");
+				if (boom_timer==-1){
+					boom_timer=0;
+					shootdire=up;
+				}
+			}
+			else{
+				obj.TNT_pos.remove(explode_thisRound.get(i));
+				obj.numTNT-=1;
+				
+				boom_timer=-1; //-1 indicates that it will not be timed until the timer is back at 0
+			}
+			//System.out.println("timerAFTER"+boom_timer);
+		}
+		if (boom_timer==-1){
+			explode_thisRound.clear();
+		}
+		if (1000<TNT_timer && TNT_timer<=3000){
+			System.out.println("TRUE");
+			//if it is time, draw the exploding TNT's and replace them with the ones that will be exploded next time the timer is at 2
+			for (int i=0;i<explode_nextRound.size();i++){
+				explode_thisRound.add(explode_nextRound.get(i));
+			}
+			explode_nextRound.clear();
+			TNT_timer=0;
+		}
+		TNT_timer++;
 	}
 }
-
