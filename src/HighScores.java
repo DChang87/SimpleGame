3892511024ff2b6d6af8d85c55e3e53511c9336c
DChang87 <diana.chang.twn@gmail.com>
2015-02-10 04:48:06 -0500

    Commented

--- src/HighScores.java
+++ src/HighScores.java
@@ -3,18 +3,17 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-
 class HighScores {
+	//this class takes care of loading the highscores and updating them
 	public HighScores(){
-		readFile();
+		readFile(); //read the old highscores file
 	}
-	private static int[] newTopScores = new int[10];
-	private static String[] newTopNames = new String[10];
-	
-	private static boolean doneLoading=false;
-	private static int [] topScores = new int[10];
-	private static String[] topNames = new String[10];
-	static FileWriter outfile;
+	private static int[] newTopScores = new int[10]; //the soon-to-be updated version of the top scores
+	private static String[] newTopNames = new String[10]; //soon to be updated version of the top names (corresponding to the scores)
+	private static boolean doneLoading=false; //if it is done loading everything
+	private static int [] topScores = new int[10]; //the current top scores
+	private static String[] topNames = new String[10]; //current top names
+	static FileWriter outfile; //the file that will be written on
 	public static void readFile(){
 		Scanner infile = null;
 		try{
@@ -24,15 +23,13 @@ class HighScores {
 			System.out.println("System cannot find HighScore.txt");
 		}
 		for (int i=0;i<10;i++){
+			//read the data and store them
 			topScores[i]=infile.nextInt();
-			System.out.println(topScores[i]);
 			topNames[i]=infile.next();
 		}
 	}
-	public static boolean returnLoading(){
-		return doneLoading;
-	}
 	public static void openNewFile(){
+		//this method opens a new file to write on
 		try{
 			outfile = new FileWriter(new File("HighScore.txt"));
 		}
@@ -43,7 +40,7 @@ class HighScores {
 	}
 	public static int placement(int score){
 		int pos=-1;
-		//returns the index: 0-9
+		//returns the index: 0-9 of where the user places on the score board compared to the top 10 scores
 		for (int i=0;i<10;i++){
 			if (topScores[i]<=score){
 				return i;
@@ -51,16 +48,28 @@ class HighScores {
 		}
 		return pos;
 	}
+	
+	//return things
 	public static int returnScore(int index){
 		return newTopScores[index];
 	}
 	public static String returnName(int index){
 		return newTopNames[index];
 	}
+	public static boolean returnLoading(){
+		return doneLoading;
+	}
+
 	public static void gameFinished(int score,String name){
+		//this method is called when the game is finished
+		//it opens the new file to write on. stores the new values to the newTopScores/newTopNames arraylists to print on screen
+		//writes the values on the new file to be accessed later
 		openNewFile();
 		int pos = placement(score);
 		if (pos!=-1){
+			//keep all of the scores before the user's score
+			//add on ther user's score
+			//take all of the scores after the user (Except the last person since the last person would be the 11th, not top 10)
 			for (int i=0;i<pos;i++){
 				tryWriting(topScores[i]+" "+topNames[i]+"\n");
 				newTopScores[i]=topScores[i];
@@ -71,7 +80,6 @@ class HighScores {
 			newTopNames[pos]=name;
 			for (int i=0;i<9-pos;i++){
 				tryWriting(topScores[pos+i]+" "+topNames[pos+i]+"\n");
-				System.out.println(pos+1+i+"POS");
 				newTopScores[pos+1+i]=topScores[pos+i];
 				newTopNames[pos+1+i]=topNames[pos+i];
 			}
@@ -85,6 +93,8 @@ class HighScores {
 		doneLoading=true;
 	}
 	public static void tryWriting(String s){
+		//this method was created so there was no need to code-out the try and catch everytime something was added to the outfile
+		//this method tries to write the string on the outfile
 		try{
 			outfile.write(s);
 		}
